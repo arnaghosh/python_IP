984749eb377cc403cd25c45fa7bf5131ea4dc910
arna <arnatubai@arnatubai-HP-Pavilion-dv6-Notebook-PC.(none)>
2014-10-14 03:20:48 +0530

    all functions modularised, but equalisation does not make changes in histogram and otsu returns slightly varied results than scikit

--- Chapter3_pt.py
+++ Chapter3_pt.py
@@ -2,12 +2,13 @@ from scipy import misc
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 import numpy as np
+from skimage.filter import threshold_otsu
 
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.557, 0.144])
 
 def histogram(gray):
-	hist,bin_edges=np.histogram(lena,range=(0,255))
+	hist,bin_edges=np.histogram(lena,bins=256,range=(0,255))
 	return hist,bin_edges
 	
 def normalisation(gray,nmin,nmax):
@@ -28,16 +29,40 @@ def binary_search(intList, intValue, lowValue, highValue):
         return binary_search(intList, intValue, lowValue, middleValue - 1)
 	return binary_search(intList, intValue, middleValue + 1, highValue)
 
+def equalisation(lena):
+	new_range=np.amax(lena)-np.amin(lena)
+	psum=np.cumsum(hist)
+	hist2=(new_range*1.0*psum)/number_of_pixels
+	lena3=lena.copy()
+	for i in range(rows):
+		for j in range(cols):
+			lena3[i,j]=hist2[lena[i,j]]
+	return lena3,hist2		
+
+def otsu(lena):
+	w=np.array([0.00 for i in range(256)])
+	w[0]=(hist[0]*1.0)/number_of_pixels
+	for i in range(255):
+		w[i+1]=w[i]+(hist[i+1]*1.0)/number_of_pixels 
+	mu=np.array([0 for i in range(256)])
+	mu[0]=0*hist[0]*1.0/number_of_pixels
+	for i in range(255):
+		mu[i+1]=mu[i]+((i+1)*hist[i+1]*1.0)/number_of_pixels
+	muT=mu[255]
+	val=((muT*w-mu)**2)/(w*(1-w))
+	val[255]=0
+	thresh=np.argmax(val)	
+	return thresh
+
 if __name__=="__main__":
-	lena=misc.imread("lena.png")
-	#lena=rgb2gray(lena)
+	lena=misc.imread("geoaware1.jpg")
+	lena=rgb2gray(lena)
 	print(lena.shape)
 	plt.imshow(lena,cmap=plt.cm.gray)
 	plt.show()
 	rows,cols = lena.shape
-	nmin=50
-	nmax=250
-	new_range=nmax-nmin
+	nmin=0
+	nmax=255
 	number_of_pixels=rows*cols
 	omax=np.amax(lena)
 	omin=np.amin(lena)
@@ -64,64 +89,28 @@ if __name__=="__main__":
 	print(hist_size)
 
 	#histogram equalisation
-
-
-	hist2=np.array([0 for i in range(256)])
-	psum=np.cumsum(hist)
-	print(bin_edges)
-	print((new_range*1.0*psum[0])/number_of_pixels)
-	for i in range(256):
-		for j in range(hist_size):
-			if(i<=bin_edges[j]):
-				hist2[i]=(new_range*1.0*psum[j-1])/number_of_pixels
-				break
-
+	
+	lena3,hist2=equalisation(lena)
+	print(hist2.size)
 	plt.plot(hist2)
 	plt.show()
-	lena3=lena.copy()
-	for i in range(rows):
-		for j in range(cols):
-			lena3[i,j]=hist2[lena[i,j]]
 	plt.imshow(lena3,cmap=plt.cm.gray)
 	plt.show()
-	hist2_,bin_edges2_=histogram(lena3)
+	hist2_,bin_edges2_=histogram(lena)
 	plt.plot(hist2_)
-	plt.show()
-	hist,bin_edges=histogram(lena)
-	plt.plot(hist)
 	plt.show()
 	print("eq done")
 
 	#OTSU thresholding
 
-	w=np.array([0.00 for i in range(256)])
-	w[0]=(hist[0]*1.0)/number_of_pixels
-	for i in range(255):
-		w[i+1]=w[i]+(hist[i+1]*1.0)/number_of_pixels 
-
-	mu=np.array([0 for i in range(256)])
-	mu[0]=1*hist[0]*1.0/number_of_pixels
-	for i in range(255):
-		mu[i+1]=mu[i]+((i+2)*hist[i+1]*1.0)/number_of_pixels
-
-	muT=mu[255]
-
-	#plt.plot(w)
-	#plt.show()
-	thresh=0
-	maxValue=0
-	for i in range(256):
-		if(w[i]==0):continue
-		val=(muT*w[i]-mu[i])*(muT*w[i]-mu[i])/(w[i]*(1.0-w[i]))
-		if(val>maxValue):
-			maxValue=val
-			thresh=i
-
+	thresh=otsu(lena)
+	thresh2=np.floor(threshold_otsu(lena))
+	print "threshold using scikit" , np.floor(threshold_otsu(lena))
 	print(thresh)
 	lena4=lena.copy()
 	for i in range(rows):
 		for j in range(cols):
-			if(lena[i,j]>=thresh):lena4[i,j]=255;
+			if(lena[i,j]>=thresh2):lena4[i,j]=255;
 			else : lena4[i,j]=0;
 
 	plt.imshow(lena4,cmap=plt.cm.gray)
